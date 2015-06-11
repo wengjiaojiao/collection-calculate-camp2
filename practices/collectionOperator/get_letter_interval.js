97c17843ce1af1d2f32339fb26c3ac7d69c26f36
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-11 09:58:35 +0800

    use range and map in oo to get letter interval

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,44 +1,17 @@
 'use strict';
-
+var _ = require('../../mylodash.js');
 function get_letter_interval(number_a, number_b) {
     var result = [];
 
     if(number_a != number_b) {
-        result = range(number_a, number_b);
+        result = _().range(number_a, number_b);
     }else {
         result.push(number_a);
     }
-    return map(result, function(n){
+    return _(result).map(function(n){
         return String.fromCharCode(n+96);
-    });
+    }).value();
 }
 
-
-
-function range(a, b) {
-    var step = Math.abs(b - a) / (b - a);
-    var count = (b- a) / step;
-    var result = [];
-
-    for (var i = 0; i <= count; i++) {
-        var item = a + i * step;
-        result.push(item);
-    }
-    return result;
-}
-function each(collection, fun) {
-    for (var i = 0; i < collection.length; i++) {
-        fun(collection[i], i);
-    }
-}
-
-function map(collection,fun) {
-    var result = [];
-
-    each(collection, function(n) {
-        result.push(fun(n));
-    });
-    return result;
-}
 
 module.exports = get_letter_interval;
