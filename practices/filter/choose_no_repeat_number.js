61bef3b4c872ee2d2dd48fb72d4a5d7804412f41
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-11 11:07:18 +0800

    use each and exist in oo to choose no repeat number

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -1,28 +1,11 @@
 'use strict';
-
+var _ = require("../../mylodash.js");
 function choose_no_repeat_number(collection) {
     var result = [];
 
-    each(collection, function(n) {
-        if(exist(result, n)) {
+    _(collection).each(function(n){
+        if(!_(result).exist(n)) {
             result.push(n);
-        }
-    });
-    return result;
-}
-
-function each(collection, fun) {
-    for (var i = 0; i < collection.length; i++) {
-        fun(collection[i], i);
-    }
-}
-
-function exist(collection, element) {
-    var result = true;
-
-    each(collection, function(n) {
-        if(n === element) {
-            result = false;
         }
     });
     return result;
