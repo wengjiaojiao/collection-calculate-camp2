6d9d7b6c5df5cc393b2f4c8d755e60ae057b889b
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-11 11:24:54 +0800

    use map function in oo to map two time

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -2,11 +2,9 @@
 var _ = require('../../mylodash.js');
 
 function map_to_even(collection){
-    return _.map(collection,twotimes);
-}
-
-function twotimes(n) {
-    return n * 2 ;
+    return _(collection).map(function(n) {
+        return n * 2 ;
+    }).value();
 }
 
 module.exports = map_to_even;
