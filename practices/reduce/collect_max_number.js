fb8e5b6b3883e6fda25beeeae94fe87fcbb46516
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-11 13:51:46 +0800

    use max function in oo to collect max

--- practices/reduce/collect_max_number.js
+++ practices/reduce/collect_max_number.js
@@ -4,10 +4,7 @@ var _ = require("../../mylodash.js");
 function collect_max_number(collection) {
 
 
-    return _.reduce(collection,max);
-}
+    return _(collection).max().value();
 
-function max(a,b) {
-    return Math.max(a,b);
 }
 module.exports = collect_max_number;
