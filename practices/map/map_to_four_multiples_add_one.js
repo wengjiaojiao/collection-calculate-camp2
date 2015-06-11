eae434ea71ab0b335cadbfa8e3a0c5e3f740b3c7
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-11 11:26:48 +0800

    use map function in oo to map to four time add one

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -2,10 +2,8 @@
 var _ = require("../../mylodash.js");
 
 var map_to_four_multiples_add_one = function(collection){
-    return _.map(collection,four);
-}
-
-function four(n) {
-    return n * 4 + 1;
+    return _(collection).map(function(n) {
+        return n * 4 + 1;
+    }).value();
  }
 module.exports = map_to_four_multiples_add_one;
