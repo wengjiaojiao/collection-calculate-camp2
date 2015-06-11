6a77f581560c9a4e7f39f5a623e54e7e11f44ba5
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-11 13:25:29 +0800

    use first function in oo to find element first i

--- practices/reduce/find_element_first_subscript.js
+++ practices/reduce/find_element_first_subscript.js
@@ -2,21 +2,8 @@
 var _ = require("../../mylodash.js");
 
 function calculate_elements_sum(collection, element) {
-    var result = 0;
-    var exist = true;
-    
-     _.foreach(collection,function(n,i){
-        if(n === element && exist) {
-            result = i;
-            exist =false;
-        }
-    });
-    return result;
+    return _(collection).first(element);
 }
 
 
 module.exports = calculate_elements_sum;
-
-
-
-//return _.indexOf(collection,element);
