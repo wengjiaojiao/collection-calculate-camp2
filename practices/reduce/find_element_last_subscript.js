f7723edd6e5619c15a5c6d5b431dfccdc2f9e723
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-11 13:28:54 +0800

    use the last function in oo to find element last i

--- practices/reduce/find_element_last_subscript.js
+++ practices/reduce/find_element_last_subscript.js
@@ -1,23 +1,9 @@
 'use strict';
+var _ = require("../../mylodash.js");
 
 function calculate_elements_sum(collection, element) {
-    var result = 0;
-    
-    each(collection, function(n, i){
-        if(n === element) {
-            result = i;
-        }
-    });
-    return result;
+    return _(collection).last(element);
 }
 
-function each(collection, fun) {
-    for (var i = 0; i < collection.length; i++) {
-        fun(collection[i], i);
-    }
-}
+
 module.exports = calculate_elements_sum;
-
-
-
-//return _.lastIndexOf(collection,element);
