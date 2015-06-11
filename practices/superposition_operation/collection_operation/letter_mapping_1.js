b622128d628db4e3534b38842ca902f5d1b06cc5
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-11 14:51:32 +0800

    even_to_letter with filter and map in prototype chain

--- practices/superposition_operation/collection_operation/letter_mapping_1.js
+++ practices/superposition_operation/collection_operation/letter_mapping_1.js
@@ -2,16 +2,11 @@
 var _ = require('../../../mylodash.js');
 
 function even_to_letter(collection) {
-    var temp = [];
-
-    temp = _.filter(collection,function(n){
-        return n % 2 === 0;
-    });
-
-    return _.map(temp,function(n){
-        return String.fromCharCode(n+96);
-
-    });
+    return _(collection).filter(function(n) {
+        return n % 2 ===0
+    }).map(function(n) {
+        return String.fromCharCode(n+96);;
+    }).value();
 }
 
 module.exports = even_to_letter;
