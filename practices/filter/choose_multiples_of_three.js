1573e16e7f5bb593af7cc84e053a994656ec08d1
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-11 10:28:26 +0800

    ues filter in oo to choose three time

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -2,25 +2,8 @@
 var _ = require('../../mylodash.js');
 
 function choose_multiples_of_three(collection) {
-
-
-    return _.filter(collection, three);
-}
-
-function three(n){
-    return n % 3 == 0;
+    return _(collection).filter(function(n) {
+        return n % 3 === 0;
+    }).value();
 }
 module.exports = choose_multiples_of_three;
-
-
-
-
-
-
-
-
-
-/*return _.remove(collection,function(n){
-    return n % 3 == 0;
-});
-*/
