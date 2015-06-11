7aff1270b3bce5ed47b126674e6d05e5cb7d58a5
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-11 11:40:39 +0800

    use the rank function in oo to rank desc

--- practices/map/rank_desc.js
+++ practices/map/rank_desc.js
@@ -2,9 +2,10 @@
 var _ = require('../../mylodash.js');
 
 var rank_desc = function(collection){
-    return _.rank(collection,function(a, b) {
+
+    return _(collection).rank(function(a, b) {
         return a > b;
-    });
+    }).value();
 }
 
 
