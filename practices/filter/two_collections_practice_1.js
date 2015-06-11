5ef8dc1803855f5113b18a6a16330b75ed885cb6
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-11 11:10:45 +0800

    use exist in oo to find the same number

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -1,36 +1,14 @@
 'use strict';
-var _ =require("../../mylodash.js");
+var _ = require("../../mylodash.js");
 
 function choose_common_elements(collection_a, collection_b) {
     var result = [];
-
-     each(collection_a, function(n) {
-         if(exist(collection_b, n)) {
-             result.push(n);
-         }
-     });
-    return result;
-}
-
-module.exports = choose_common_elements;
-
-function each(collection, fun){
-    for (var i = 0; i < collection.length; i++) {
-        fun(collection[i], i);
-    }
-}
-
-function exist(collection, element) {
-    var result = false;
-
-    each(collection, function(n) {
-        if(n === element) {
-            result = true;
+    _(collection_a).each(function(n) {
+        if(_(collection_b).exist(n)) {
+            result.push(n);
         }
     });
     return result;
 }
 
-
-
-//return _.intersection(collection_a,collection_b);
+module.exports = choose_common_elements;
