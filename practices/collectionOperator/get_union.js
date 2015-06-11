e4a091332ec6106524b3ba516476c2f782d0994f
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-11 10:08:08 +0800

    use each and exist in oo to get union

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -3,29 +3,40 @@
 function get_union(collection_a, collection_b) {
     var result = collection_a;
 
-    each(collection_b, function(n) {
-        if(exist(result, n)) {
+    _(collection_b).each(function(n) {
+        if(!_(result).exist(n)) {
             result.push(n);
         }
     });
     return result;
 }
 
-function each(collection, fun) {
-    for (var i = 0; i < collection.length; i++) {
-        fun(collection[i],i);
+module.exports = get_union;
+
+function _ (collection) {
+    if(!(this instanceof _)) {
+        return new _(collection);
+    }
+    this.collection = collection;
+}
+
+_.prototype = {
+    contructor:_,
+    each:function(fun) {
+        for (var i = 0; i < this.collection.length; i++) {
+            fun(this.collection[i]);
+        }
+    },
+    exist:function(element) {
+        var result = false;
+        this.each(function(n, i) {
+            if(n === element) {
+                result = true;
+            }
+        });
+        return result;
+    },
+    value:function() {
+        return this.collection;
     }
 }
-
-function exist(collection, element) {
-    var result = true;
-
-    each(collection, function(n) {
-        if(n === element) {
-            result = false;
-        }
-    });
-    return result;
-}
-
-module.exports = get_union;
